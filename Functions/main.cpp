1467214897efa0b26f37d777ad98b76104db3b12
NastyaSkv <NastyaSkv@DESKTOP-S0J6MVO>
2022-07-08 21:11:10 +0300

    Encoding

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,10 +1,43 @@
-//Functions
+﻿//Functions
 
 #include<iostream>
 using namespace std;
+
+int Add(int a, int b) //функция сложения 2х чисел a и b
+{
+	int c = a + b;
+	return(c);
+}
+int Sub(int a, int b)
+{
+	return a - b;
+}
+
+int Mul(int a, int b)
+{
+	return a * b;
+}
+
+int Div(int a, int b)
+{
+	return a / b;
+}
 
 void main()
 {
 	setlocale(LC_ALL, "");
 
+	//синтаксис функции:
+	   /*type name(parameters)
+	   {
+	      	...;
+		    group - of - statements;
+		    ...;
+	   }*/
+
+	int a, b;
+	cout << "Введите 2 числа: "; cin >> a >> b;
+	int c = Add(a, b); //присваиваем переменной с значение функции
+	cout << a << " + " << b <<" = " << c << endl;
+	cout << a << " - " << b << " = " << Sub(a, b) << endl; //теперь вызовем не переменную, а саму функцию
 }
